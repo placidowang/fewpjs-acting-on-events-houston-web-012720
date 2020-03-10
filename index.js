74c533c2f548b6639410531eb4a49cdadd631d2c
placidowang <placidow@gmail.com>
2020-03-10 13:35:07 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Your code here
+let dodger = document.getElementById("dodger");
+let div = document.getElementById('game');
+
+function moveDodgerLeft() {
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+function moveDodgerRight() {
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
+
+  // if (left < (div.offsetWidth - dodger.offsetWidth)) {
+  if (left < 360) {
+    dodger.style.left = `${left + 1}px`;
+  }
+}
+
+document.addEventListener("keydown", function(e) {
+  switch (e.key) {
+    case "ArrowLeft":
+      moveDodgerLeft();
+      break
+    case "ArrowRight":
+      moveDodgerRight();
+      break
+  }
+});
